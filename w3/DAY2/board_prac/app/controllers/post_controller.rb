06431319f1faab38f6a8a6bcdff25defd75eb7e5
JSCho <teamma@naver.com>
2017-12-19 18:21:49 +0900

    practice add

---
+++ w3/DAY2/board_prac/app/controllers/post_controller.rb
@@ -0,0 +1,45 @@
+class PostController < ApplicationController
+  def index
+    @posts = Post.all.reverse
+  end
+
+  def new
+    # form에서 쓰는 창이니까 아무것도 없다!
+  end
+
+  def create
+
+    Post.create(
+      username: params[:username],
+      title: params[:title],
+      content: params[:content]
+    )
+
+    redirect_to '/'
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+  def destroy
+    Post.find(params[:id]).destroy
+    redirect_to '/'
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(
+      username: params[:username],
+      title: params[:title],
+      content: params[:content]
+    )
+
+    redirect_to "/post/show/#{params[:id]}"
+  end
+end
