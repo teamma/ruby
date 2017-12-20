8c957d75d1b1d4c9f18f05d27fd4844602bd4ee4
JSCho <teamma@naver.com>
2017-12-20 11:43:56 +0900

    get->post modified

--- w3/DAY2/board_prac/config/routes.rb
+++ w3/DAY2/board_prac/config/routes.rb
@@ -5,15 +5,15 @@ Rails.application.routes.draw do
 
   get 'post/new'
 
-  get 'post/create'
+  post 'post/create'
 
   get 'post/show/:id' => 'post#show'
 
-  get 'post/destroy/:id' => 'post#destroy'
+  delete 'post/destroy/:id' => 'post#destroy'
 
   get 'post/edit/:id' => 'post#edit'
 
-  get 'post/update/:id' => 'post#update'
+  post 'post/update/:id' => 'post#update'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
